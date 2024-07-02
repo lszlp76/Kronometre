bb37dcf4753c3d6bdab4d9a4b4012ac88b397efc
LsZlp <uozalp.uo@gmail.com>
2024-07-03 02:29:41 +0300

    çalışır hale getirildi

---
+++ app/src/main/java/com/lszlp/choronometre/Lap.java
@@ -0,0 +1,15 @@
+package com.lszlp.choronometre;
+
+public class Lap {
+
+    public String unit;
+    public int lapsayisi;
+    public String lap;
+
+
+    public Lap(int lapsayisi, String unit,String lap) {
+        this.unit = unit;
+        this.lapsayisi = lapsayisi;
+        this.lap = lap;
+    }
+}
---
+++ app/src/main/java/com/lszlp/choronometre/LapListAdapter.java
@@ -0,0 +1,51 @@
+package com.lszlp.choronometre;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.lszlp.choronometre.databinding.LaprowsBinding;
+
+import java.util.ArrayList;
+
+public class LapListAdapter extends RecyclerView.Adapter<LapListAdapter.LapListViewHolder>{
+    ArrayList<Lap> lapArrayList;
+    public LapListAdapter(ArrayList<Lap> lapArrayList) {
+        this.lapArrayList = lapArrayList;
+    }
+
+
+    @NonNull
+    @Override
+    public LapListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+      LaprowsBinding laprowsBinding = LaprowsBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
+        return new LapListViewHolder(laprowsBinding);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull LapListViewHolder holder, int position) {
+        holder.itemView.laprow.setText(lapArrayList.get(position).unit);
+        holder.itemView.laprow2.setText(String.valueOf(lapArrayList.get(position).lapsayisi));
+        holder.itemView.laprow3.setText(lapArrayList.get(position).lap);
+    }
+    @Override
+    public int getItemCount() {
+        return lapArrayList.size();
+    }
+
+    public class LapListViewHolder extends RecyclerView.ViewHolder{
+
+
+private LaprowsBinding itemView;
+
+        public LapListViewHolder(@NonNull LaprowsBinding itemView) {
+            super(itemView.getRoot());
+            this.itemView = itemView;
+        }
+    }
+
+
+}
